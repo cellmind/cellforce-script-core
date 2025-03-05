91b94c6d52fc572a1ed45db8b465f2901689512e
elans2 <elans2.gu@gmail.com>
2025-03-05 14:48:23 +0800

    fix golang panic

--- src/runner/golang.rs
+++ src/runner/golang.rs
@@ -1,8 +1,18 @@
 
+use std::panic;
+use std::any::Any;
 use crate::runner::base::ScriptFunctionRunner;
 use crate::errors::ScriptError;
 
 use crate::runner::go::interpreter::{FreeGoScriptInterpreterRequest, GoScriptCall, GoScriptCallImpl, MapInStrOutStrRequest, NewGoScriptInterpreterRequest};
+
+
+fn box_to_string(boxed: Box<dyn Any + Send>) -> Option<String> {
+    if let Some(string) = boxed.downcast_ref::<String>() {
+        return Some(string.clone());
+    }
+    Some(format!("{:?}", boxed))
+}
 
 pub struct GolangFunctionRunner {
     ptr_offset: i32,
@@ -18,7 +28,14 @@ impl GolangFunctionRunner {
             function: func.to_string(),
             script: go_script.to_string()
         };
-        let resp = GoScriptCallImpl::new_interpreter(&req);
+
+        let resp = panic::catch_unwind(|| {
+            GoScriptCallImpl::new_interpreter(&req)
+        }).map_err(|e| {
+            ScriptError::GeneralError {
+                msg: format!("failed to create interpreter: {:?}", box_to_string(e).unwrap_or("failed to create interpreter".to_string())),
+            }
+        })?;
         if resp.error != "" {
             return Err(ScriptError::GeneralError {
                 msg: format!("failed to create interpreter: {:?}", resp.error),
@@ -36,9 +53,18 @@ impl Drop for GolangFunctionRunner {
         let req = FreeGoScriptInterpreterRequest {
             ptr_offset: self.ptr_offset
         };
-        let resp = GoScriptCallImpl::free_interpreter(&req);
-        if resp.error != "" {
-            eprintln!("failed to free interpreter: {:?}", resp.error);
+        let result = panic::catch_unwind(|| {
+            GoScriptCallImpl::free_interpreter(&req)
+        });
+        match result {
+            Ok(resp) => {
+                if resp.error != "" {
+                    eprintln!("failed to free interpreter: {:?}", resp.error);
+                }
+            }
+            Err(e) => {
+                eprintln!("failed to free interpreter: {:?}", box_to_string(e).unwrap_or("failed to free interpreter".to_string()));
+            }
         }
     }
 }
@@ -49,7 +75,13 @@ impl ScriptFunctionRunner for GolangFunctionRunner {
             ptr_offset: self.ptr_offset,
             value: value.to_string()
         };
-        let resp = GoScriptCallImpl::map_in_str_out_str(&req);
+        let resp = panic::catch_unwind(|| {
+            GoScriptCallImpl::map_in_str_out_str(&req)
+        }).map_err(|e| {
+            ScriptError::GeneralError {
+                msg: format!("failed to call function: {:?}", box_to_string(e).unwrap_or("failed to call function".to_string())),
+            }
+        })?;
         if resp.error != "" {
             return Err(ScriptError::GeneralError {
                 msg: format!("failed to call function: {:?}", resp.error),
